256449cad34c6159d501868810ba99ef2525a7ab
Mattias.De.Wael <mattias.de.wael@howest.be>
2018-09-24 09:38:07 +0200

    Updated domain classes to db

---
+++ src/main/java/ui/TableApp.java
@@ -0,0 +1,26 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TableApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/tablePanel.fxml"));
+
+        Scene scene = new Scene(root);
+
+
+        stage.setTitle("All passwords");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
